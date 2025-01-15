dad6cead60e87121e612a7cca05ff437aa1305a7
Thollarkings <mediaxtreme1@gmail.com>
2025-01-15 06:04:41 -0500

    interface tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,96 +39,113 @@ const Home = () => {
         <p><strong>Technical Skills:</strong> HTML, CSS, JavaScript, React, Python, Node.js, Express, Git, GitHub, AWS, IBM Cloud, Figma ... . . .  .  .  .</p>
         <p><strong>Soft Skills:</strong> Problem-solving, teamwork, communication, project management, UI/UX, web design, graphic design ... . . .  .  .  .</p>
       </div>
+
+      
+
       <div className="projects-container">
-        <h1>My Projects</h1>
-        <ul className="projects-list">
-          <li>
-            <a href="https://bit.ly/invoicemax" target="_blank" rel="noopener noreferrer">
-              <img
-                src={Invoicemax}
-                alt="InvoiceMax App Screenshot"
-                className="project-image" />
-              <br />
-            </a>
-            <a href="https://bit.ly/invoicemax"><b>InvoiceMax</b></a>
-            <p>Comprehensive invoicing application using React.</p>
-          </li>
-          <li>
-            <a href="https://rebrand.ly/tailors-log" target="_blank" rel="noopener noreferrer">
-              <img
-                src={Tailorslogs}
-                alt="Tailor's Logbook App Screenshot"
-                className="project-image" />
-              <br />
-            </a>
-            <a href="https://rebrand.ly/tailors-log"><b>My Tailor's Logbook</b></a>
-            <p>
-              This app is an essential tool for accurately recording client measurements,
-              tracking key details, and ensuring precise fittings and timely delivery.
-            </p>
-          </li>
-          <li>
-            <a href="https://thollarkings.github.io/ejtos-react_budget_app/" target="_blank" rel="noopener noreferrer">
-              <img
-                src={Budgetapp}
-                alt="Budget App Screenshot"
-                className="project-image" />
-              <br />
-            </a>
-            <a href="https://thollarkings.github.io/ejtos-react_budget_app/"><b>Budget App</b></a>
-            <p>
-              User-friendly budget management tool with React. This app is an essential
-              tool for accurately recording client measurements, tracking key details, and<br />
-              ensuring precise fittings and timely delivery.
-            </p>
-          </li>
-          <li>
-            <a href="https://thollarkings.github.io/Tollars-Calculator/" target="_blank" rel="noopener noreferrer">
-              <img
-                src={Simplecalculator}
-                alt="Kings-Calc App Screenshot"
-                className="project-image" />
-              <br />
-            </a>
-            <a href="https://thollarkings.github.io/Tollars-Calculator/"><b>Kings-Calc</b></a>
-            <p>
-              This app is a Simplest Working Calculator designed with React. Its primary
-              goal is to provide basic arithmetic operations and allow users to interact with<br />
-              a clean, user-friendly interface.
-            </p>
-          </li>
-          <li>
-            <a href="https://thollarkings.github.io/Tailors_Log_Book/" target="_blank" rel="noopener noreferrer">
-              <img
-                src={Measurementrecorder}
-                alt="Measurements Recorder App Screenshot"
-                className="project-image" />
-              <br />
-            </a>
-            <a href="https://thollarkings.github.io/Tailors_Log_Book/"><b>Measurements Recorder</b></a>
-            <p>
-              This application serves as a measurement tracking tool, similar to the
-              My Tailor's Logbook app, this was developed using vanilla JavaScript.
-            </p>
-          </li>
-          <li>
-            <a href="https://thollarkings.github.io/Tailors_Log_Book/" target="_blank" rel="noopener noreferrer">
-              <img
-                src={Unitconverter}
-                alt="Unit Converter App Screenshot"
-                className="project-image" />
-              <br />
-            </a>
-            <a href="https://thollarkings.github.io/Tailors_Log_Book/"><b>Unit Converter</b></a>
-            <p>
-              Unit Converter Web App is an interactive application designed to simplify
-              the process of converting values between different units of measurement.<br />
-              Using vanilla JavaScript, it provides a responsive and user-friendly interface
-              where users can input values in one unit and instantly see the equivalent value<br />
-              in another.
-            </p>
-          </li>
-        </ul>
+      <h1 className='projtit'>My Projects</h1>
+        <div className="project-item">
+          <a href="https://bit.ly/invoicemax" target="_blank" rel="noopener noreferrer">
+            <img
+              src={Invoicemax}
+              alt="InvoiceMax App Screenshot"
+              className="project-image"
+            />
+          </a>
+          <a href="https://bit.ly/invoicemax">
+            <h2>InvoiceMax</h2>
+          </a>
+          <p>Comprehensive invoicing application using React.</p>
+        </div>
+
+        <div className="project-item">
+          <a href="https://rebrand.ly/tailors-log" target="_blank" rel="noopener noreferrer">
+            <img
+              src={Tailorslogs}
+              alt="Tailor's Logbook App Screenshot"
+              className="project-image"
+            />
+          </a>
+          <a href="https://rebrand.ly/tailors-log">
+            <h2>My Tailor's Logbook</h2>
+          </a>
+          <p>
+            This app is an essential tool for accurately recording client measurements,
+            tracking key details, and ensuring precise fittings and timely delivery.
+          </p>
+        </div>
+
+        <div className="project-item">
+          <a href="https://thollarkings.github.io/ejtos-react_budget_app/" target="_blank" rel="noopener noreferrer">
+            <img
+              src={Budgetapp}
+              alt="Budget App Screenshot"
+              className="project-image"
+            />
+          </a>
+          <a href="https://thollarkings.github.io/ejtos-react_budget_app/">
+            <h2>Budget App</h2>
+          </a>
+          <p>
+            User-friendly budget management tool with React. This app is an essential
+            tool for accurately recording client measurements, tracking key details, and
+            ensuring precise fittings and timely delivery.
+          </p>
+        </div>
+        <div className="project-item">
+          <a href="https://thollarkings.github.io/Tailors_Log_Book/" target="_blank" rel="noopener noreferrer">
+            <img
+              src={Measurementrecorder}
+              alt="Measurements Recorder App Screenshot"
+              className="project-image"
+            />
+          </a>
+          <a href="https://thollarkings.github.io/Tailors_Log_Book/">
+            <h2>Measurements Recorder</h2>
+          </a>
+          <p>
+            This application serves as a measurement tracking tool. Similar to the
+            My Tailor's Logbook app, this was developed using vanilla JavaScript.
+          </p>
+        </div>
+
+        <div className="project-item">
+          <a href="https://thollarkings.github.io/Tollars-Calculator/" target="_blank" rel="noopener noreferrer">
+            <img
+              src={Simplecalculator}
+              alt="Kings-Calc App Screenshot"
+              className="project-image"
+            />
+          </a>
+          <a href="https://thollarkings.github.io/Tollars-Calculator/">
+            <h2>Kings-Calc</h2>
+          </a>
+          <p>
+            This app is a Simple Working Calculator designed with React. Its primary
+            goal is to provide basic arithmetic operations.
+          </p>
+        </div>
+
+        <div className="project-item">
+          <a href="https://thollarkings.github.io/Tailors_Log_Book/" target="_blank" rel="noopener noreferrer">
+            <img
+              src={Unitconverter}
+              alt="Unit Converter App Screenshot"
+              className="project-image"
+            />
+          </a>
+          <a href="https://thollarkings.github.io/Tailors_Log_Book/">
+            <h2>Unit Converter</h2>
+          </a>
+          <p>
+            Unit Converter Web App is an interactive application designed to simplify
+            the process of converting values between different units of measurement.
+            Using vanilla JavaScript, it provides a responsive and user-friendly interface
+            where users can input values in one unit and instantly see the equivalent value
+            in another.
+          </p>
+        </div>
+
       </div>
     </div>
   );
